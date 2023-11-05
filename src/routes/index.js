c6f7d6b2103443d73dff71c59dd095beb8c92c57
Achieve-software <vtcbcdtyr@gmail.com>
2023-11-06 00:41:24 +0300

    car

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,13 @@ const router = require('express').Router()
 // routes/:
 
 // URL: /
-
+//user:
+router.use('/users', require('./user'))
+// car:
+router.use('/cars', require('./car'))
 // document:
 router.use('/documents', require('./document'))
+
 
 /* ------------------------------------------------------- */
 module.exports = router
