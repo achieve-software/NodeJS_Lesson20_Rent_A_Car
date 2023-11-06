ba86ec4761a8e3144c63070b0355f6602e347adc
Achieve-software <vtcbcdtyr@gmail.com>
2023-11-06 23:50:56 +0300

    permission

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -7,20 +7,26 @@
 module.exports = {
 
     isLogin: (req, res, next) => {
+        // any User:
+        if (req.user && req.user.isActive) {
 
-        if (req.user) {
             next()
+
         } else {
+
             res.errorStatusCode = 403
             throw new Error('NoPermission: You must login.')
         }
     },
 
     isAdmin: (req, res, next) => {
+        // only Admin:
+        if (req.user && req.user.isActive && req.user.isAdmin) {
 
-        if (req.user && req.user.isAdmin) {
             next()
+
         } else {
+
             res.errorStatusCode = 403
             throw new Error('NoPermission: You must login and to be Admin.')
         }
